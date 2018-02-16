a174d62bab3df431ac7ec1bfb6bd968df1b1e1bc
Xavier Inuca <bryan_xavi25@hotmail.com>
2018-02-16 10:57:29 -0500

    Modificacion
    Index

--- login.php
+++ login.php
@@ -22,7 +22,7 @@ $login = new Login();
 if ($login->isUserLoggedIn() == true) {
     // the user is logged in. you can do whatever you want here.
     // for demonstration purposes, we simply show the "you are logged in" view.
-   header("location: facturas.php");
+   header("location: usuarios.php");
 
 } else {
     // the user is not logged in. you can do whatever you want here.
