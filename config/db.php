3ff552ae86a2e5840947327fe4b1346ac15251ad
Xavier Inuca <bryan_xavi25@hotmail.com>
2018-02-16 12:12:49 -0500

    Conexion
    Conexion a la base de datos en la Nube

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 /*Datos de conexion a la base de datos*/
-define('DB_HOST', 'servidorutncloud.database.windows.net');//DB_HOST:  generalmente suele ser "127.0.0.1"
-define('DB_USER', 'utn');//Usuario de tu base de datos
-define('DB_PASS', '1994Xavier');//Contraseña del usuario de la base de datos
-define('DB_NAME', 'facturacion');//Nombre de la base de datos
+define('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'ba78fed948b3f9');//Usuario de tu base de datos
+define('DB_PASS', '4bf7c3d3');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'heroku_bed8d7e563b7b30');//Nombre de la base de datos
 ?>
